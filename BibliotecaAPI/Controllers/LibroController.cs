89180a3c05ad44e51c3e2db8bea70ea4b49cf7e9
Carlos27-design <coronadoc899@gmail.com>
2026-03-14 19:45:48 -0300

    Agregar archivos de proyecto.

---
+++ BibliotecaAPI/Controllers/LibroController.cs
@@ -0,0 +1,82 @@
+﻿using BibliotecaAPI.Data;
+using BibliotecaAPI.Enitities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/libro")]
+    public class LibroController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public LibroController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Libro>> Get() 
+        {
+            return await context.Libros.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            var libro = await context.Libros
+                .Include(x => x.Autor)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (libro is null)
+            {
+                return NotFound();
+            }
+            return libro;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Libro libro)
+        {
+            var existeAutor = await context.Autores.AnyAsync(x => x.Id == libro.AutorId);
+            if (existeAutor)
+            {
+                return BadRequest($"El autor de id {libro.AutorId} no existe");
+            }
+            context.Add(libro);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Libro libro)
+        {
+            if (id != libro.Id)
+            {
+                return BadRequest("Los ids no coinciden");
+            }
+
+            var existeAutor = await context.Autores.AnyAsync(x => x.Id == libro.AutorId);
+
+            if (!existeAutor)
+            {
+                return BadRequest($"El autor de id {libro.AutorId}");
+            }
+
+            context.Update(libro);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var registrosBorrados = await context.Libros.Where(x => x.Id == id).ExecuteDeleteAsync();
+            if(registrosBorrados == 0)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ BibliotecaAPI/Program.cs
@@ -0,0 +1,22 @@
+using BibliotecaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//área de servicios;
+
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+var app = builder.Build();
+
+// area de middlewares
+
+app.MapControllers();
+
+app.Run();
